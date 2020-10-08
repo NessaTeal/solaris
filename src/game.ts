bc57887da53660f03b795a75dbba452688bfe346
Vadim Prokopev <vadimjprokopev@gmail.com>
2020-10-08 21:15:06 +0300

    Add linting

--- src/game.ts
+++ src/game.ts
@@ -1,29 +1,25 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
-    }
+export default class Demo extends Phaser.Scene {
+  constructor() {
+    super('demo');
+  }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-    }
+  preload(): void {
+    this.load.image('logo', 'assets/phaser3-logo.png');
+  }
 
-    create ()
-    {
-        const logo = this.add.image(206, 46, 'logo');
-    }
+  create(): void {
+    this.add.image(206, 46, 'logo');
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#123456',
-    width: 413,
-    height: 93,
-    scene: Demo
+  type: Phaser.AUTO,
+  backgroundColor: '#123456',
+  width: 413,
+  height: 93,
+  scene: Demo,
 };
 
-const game = new Phaser.Game(config);
+new Phaser.Game(config);
