48c6d50aab144261f717d2a59387306ec6faf58d
Vadim Prokopev <vadimjprokopev@gmail.com>
2020-10-12 04:12:21 +0300

    Show planet statistics on hover

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,18 @@
 import 'phaser';
-import PlanetSystem from './planet-system';
+import PlanetSystem, {
+  EXPLORABLE_AREA_KEY,
+  HABITABLE_AREA_KEY,
+  HIDE_SYSTEM_STATS_EVENT,
+  SHOW_SYSTEM_STATS_EVENT,
+} from './planet-system';
 
-export default class Demo extends Phaser.Scene {
+export default class Main extends Phaser.Scene {
+  stats: Phaser.GameObjects.Container;
+  statsHabitableArea: Phaser.GameObjects.Text;
+  statsExplorableArea: Phaser.GameObjects.Text;
+
   constructor() {
-    super('demo');
+    super('main');
   }
 
   preload(): void {
@@ -13,6 +22,59 @@ export default class Demo extends Phaser.Scene {
   create(): void {
     const planetSystem = new PlanetSystem(this, 100, 100);
     this.add.existing(planetSystem);
+    const planetSystem2 = new PlanetSystem(this, 400, 400);
+    this.add.existing(planetSystem2);
+
+    const statsHabitableAreaLabel = new Phaser.GameObjects.Text(
+      this,
+      30,
+      0,
+      'Habitable area:',
+      {
+        fontFamily: 'Arial',
+        color: '#00ddff',
+        fontSize: '18px',
+      },
+    );
+    this.statsHabitableArea = new Phaser.GameObjects.Text(this, 230, 0, '', {
+      fontFamily: 'Arial',
+      color: '#00ddff',
+      fontSize: '18px',
+    });
+
+    const statsExplorableAreaLabel = new Phaser.GameObjects.Text(
+      this,
+      30,
+      50,
+      'Explorable area:',
+      {
+        fontFamily: 'Arial',
+        color: '#00ddff',
+        fontSize: '18px',
+      },
+    );
+    this.statsExplorableArea = new Phaser.GameObjects.Text(this, 230, 50, '', {
+      fontFamily: 'Arial',
+      color: '#00ddff',
+      fontSize: '18px',
+    });
+    this.stats = new Phaser.GameObjects.Container(this, 0, 800, [
+      statsHabitableAreaLabel,
+      this.statsHabitableArea,
+      statsExplorableAreaLabel,
+      this.statsExplorableArea,
+    ]);
+    this.stats.setVisible(false);
+    this.add.existing(this.stats);
+
+    this.events.on(SHOW_SYSTEM_STATS_EVENT, () => {
+      this.statsHabitableArea.setText(this.registry.get(HABITABLE_AREA_KEY));
+      this.statsExplorableArea.setText(this.registry.get(EXPLORABLE_AREA_KEY));
+      this.stats.setVisible(true);
+    });
+    this.events.on(HIDE_SYSTEM_STATS_EVENT, () => {
+      this.stats.setVisible(false);
+    });
   }
 }
 
@@ -21,7 +83,7 @@ const config = {
   backgroundColor: '#123456',
   width: 1000,
   height: 1000,
-  scene: Demo,
+  scene: Main,
 };
 
 new Phaser.Game(config);
--- src/planet-system.ts
+++ src/planet-system.ts
@@ -2,18 +2,53 @@ import 'phaser';
 
 const RADIUS = 50;
 
-export default class PlanetSystem extends Phaser.GameObjects.Ellipse {
+export const SHOW_SYSTEM_STATS_EVENT = 'show_system_stats';
+export const HIDE_SYSTEM_STATS_EVENT = 'hide_system_stats';
+export const HABITABLE_AREA_KEY = 'habitable_area';
+export const EXPLORABLE_AREA_KEY = 'explorable_area';
+
+export default class PlanetSystem extends Phaser.GameObjects.Container {
+  planet: Phaser.GameObjects.Ellipse;
+  habitableArea: number;
+  explorableArea: number;
+
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, RADIUS * 2, RADIUS * 2, 0x00a050);
-    this.on(
+    super(scene, x, y);
+    this.configurePlanet(scene);
+    this.habitableArea = Phaser.Math.Between(100, 200);
+    this.explorableArea = Phaser.Math.Between(300, 400);
+  }
+
+  configurePlanet(scene: Phaser.Scene): void {
+    this.planet = new Phaser.GameObjects.Ellipse(
+      scene,
+      0,
+      0,
+      RADIUS * 2,
+      RADIUS * 2,
+      0x00a050,
+    );
+    this.add(this.planet);
+
+    this.planet.on(
       'pointerdown',
       () => {
         alert('Click');
       },
       scene,
     );
-    this.setInteractive(
-      new Phaser.Geom.Circle(x - RADIUS, y - RADIUS, RADIUS),
+
+    this.planet.on('pointerover', () => {
+      scene.registry.set(HABITABLE_AREA_KEY, this.habitableArea);
+      scene.registry.set(EXPLORABLE_AREA_KEY, this.explorableArea);
+      scene.events.emit(SHOW_SYSTEM_STATS_EVENT);
+    });
+    this.planet.on('pointerout', () => {
+      scene.events.emit(HIDE_SYSTEM_STATS_EVENT);
+    });
+
+    this.planet.setInteractive(
+      new Phaser.Geom.Circle(RADIUS, RADIUS, RADIUS),
       Phaser.Geom.Circle.Contains,
     );
   }
